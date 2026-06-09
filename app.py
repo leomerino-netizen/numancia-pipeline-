af48a4b3183c3dbfeefe0e9f49ce0b71d4b0deca
leomerino-netizen <leo.merino@printcolorweb.com>
2026-06-09 09:48:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -582,4 +582,6 @@ def procesar_manuscrito():
 
         asesora    = request.form.get('asesora', 'laura')
         titulo_ovr = request.form.get('titulo', '')
-        autor_ovr  =
+      autor_ovr = None
+# o
+autor_ovr = request.form.get("autor_ovr", "")
